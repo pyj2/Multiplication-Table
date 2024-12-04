22eea143bdffaf226407321929ef2bea8ed9ae5e
PYJ <bagyj12345@gmail.com>
2024-12-04 20:49:19 +0900

    구구단 출력 라벨 추가
    @단 추가 : 단별로 구분되기 위함

--- Mutiplication_Table/main.c
+++ Mutiplication_Table/main.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 int main() {
     for (int i = 1; i <= 9; i++) {
         for (int j = 1; j <= 9; j++) {
             printf("%d x %d = %d\n", i, j, i * j);
         }
+        printf("==== %d 단 ====\n", i);
         printf("\n");
     }
     return 0;
