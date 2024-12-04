2138d1a9a96e598b8666e05c6efaa524f96df31c
PYJ <bagyj12345@gmail.com>
2024-12-04 20:57:58 +0900

    사용자 입력 기능 추가
    시작/끝 단 범위 설정 추가: 사용자 편리성 위함

--- Mutiplication_Table/main.c
+++ Mutiplication_Table/main.c
@@ -1,6 +1,9 @@
 ﻿#include <stdio.h>
 int main() {
-    for (int i = 1; i <= 9; i++) {
+    int start, end;
+    printf("시작 단과 끝 단을 입력하세요: ");
+    scanf_s("%d %d", &start, &end); 
+    for (int i = start; i <= end; i++) {
         for (int j = 1; j <= 9; j++) {
             printf("%2d x %2d = %2d\n", i, j, i * j);
         }
