4fb19ce27a37cc9042ff5c31176abda91fcbc078
PYJ <bagyj12345@gmail.com>
2024-12-04 21:27:20 +0900

    예외 처리 기능 추가2
    시작 단이 끝 단보다 큰 경우 오류 메시지 출력: 사용자 편리성 위함

--- Mutiplication_Table/main.c
+++ Mutiplication_Table/main.c
@@ -5,12 +5,17 @@ int main() {
     printf("시작 단과 끝 단을 입력하세요: ");
     // 예: 시작 단과 끝 단을 입력하세요: 2 5
     // 2단부터 5단까지 출력됩니다.
-    if (scanf_s("%d %d", &start, &end) != 2) { 
+    if (scanf_s("%d %d", &start, &end) != 2) {
         printf("숫자를 입력하세요.\n");
         return 1;
     }
 
-    if (start < 1 || end > 9 || start > end) {
+    if (start > end) { 
+        printf("시작 단이 끝 단보다 클 수 없습니다.\n");
+        return 1;
+    }
+
+    if (start < 1 || end > 9) {
         printf("잘못된 입력입니다. 1~9 범위의 값을 올바르게 입력하세요.\n");
         return 1;
     }
