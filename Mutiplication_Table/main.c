a27e8f2f4eba95ec504e83c7d84e740e3be72abe
PYJ <bagyj12345@gmail.com>
2024-12-04 21:02:15 +0900

    사용자 입력 기능 추가2
    숫자 범위 초과 시 예외 처리 추가: 사용자 편리성 위함

--- Mutiplication_Table/main.c
+++ Mutiplication_Table/main.c
@@ -1,13 +1,20 @@
 ﻿#include <stdio.h>
+
 int main() {
     int start, end;
     printf("시작 단과 끝 단을 입력하세요: ");
-    scanf_s("%d %d", &start, &end); 
+    scanf_s("%d %d", &start, &end);
+
+    if (start < 1 || end > 9 || start > end) {
+        printf("잘못된 입력입니다. 1~9 범위의 값을 올바르게 입력하세요.\n");
+        return 1;
+    }
+
     for (int i = start; i <= end; i++) {
+        printf("==== %d 단 ====\n", i);
         for (int j = 1; j <= 9; j++) {
             printf("%2d x %2d = %2d\n", i, j, i * j);
         }
-        printf("==== %d 단 ====\n", i);
         printf("\n");
     }
     return 0;
