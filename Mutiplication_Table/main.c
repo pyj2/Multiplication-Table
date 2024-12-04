344094296c73cbb3abf0caeee18907b78f612584
PYJ <bagyj12345@gmail.com>
2024-12-04 21:51:50 +0900

    출력 메시지 기능 추가
    오름차순/내림차순 출력 기능 추가: 가독성 향상 위함

--- Mutiplication_Table/main.c
+++ Mutiplication_Table/main.c
@@ -1,11 +1,31 @@
 ﻿#include <stdio.h>
+
 int main() {
-    for (int i = 1; i <= 9; i++) {
-        for (int j = 1; j <= 9; j++) {
-            printf("%2d x %2d = %2d\n", i, j, i * j);
+    int order;
+    printf("출력 순서를 선택하세요 (1: 오름차순, 2: 내림차순): ");
+    scanf_s("%d", &order);
+
+    if (order == 1) { // 오름차순 출력
+        for (int i = 1; i <= 9; i++) {
+            printf("==== %d 단 ====\n", i);
+            for (int j = 1; j <= 9; j++) {
+                printf("%2d x %2d = %2d\n", i, j, i * j);
+            }
+            printf("\n");
         }
-        printf("==== %d 단 ====\n", i);
-        printf("\n");
     }
+    else if (order == 2) { // 내림차순 출력
+        for (int i = 9; i >= 1; i--) {
+            printf("==== %d 단 ====\n", i);
+            for (int j = 1; j <= 9; j++) {
+                printf("%2d x %2d = %2d\n", i, j, i * j);
+            }
+            printf("\n");
+        }
+    }
+    else {
+        printf("잘못된 입력입니다. 프로그램을 종료합니다.\n");
+    }
+
     return 0;
 }
