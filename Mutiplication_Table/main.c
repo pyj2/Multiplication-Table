723a79fe3b4f32a7346277a971e89a87ef0ec318
PYJ <bagyj12345@gmail.com>
2024-12-04 21:30:07 +0900

    예외 처리 기능 추가3
    예외 처리 로직 함수화: 코드 용이성 향상 위함

--- Mutiplication_Table/main.c
+++ Mutiplication_Table/main.c
@@ -1,5 +1,20 @@
 ﻿#include <stdio.h>
 
+int validate_input(int start, int end) { 
+    if (start < 1 || end > 9 || start > end) return 0;
+    return 1;
+}
+
+void print_table(int start, int end) { 
+    for (int i = start; i <= end; i++) {
+        printf("==== %d 단 ====\n", i);
+        for (int j = 1; j <= 9; j++) {
+            printf("%2d x %2d = %2d\n", i, j, i * j);
+        }
+        printf("\n");
+    }
+}
+
 int main() {
     int start, end;
     printf("시작 단과 끝 단을 입력하세요: ");
@@ -10,22 +25,11 @@ int main() {
         return 1;
     }
 
-    if (start > end) { 
-        printf("시작 단이 끝 단보다 클 수 없습니다.\n");
-        return 1;
-    }
-
-    if (start < 1 || end > 9) {
+    if (!validate_input(start, end)) {
         printf("잘못된 입력입니다. 1~9 범위의 값을 올바르게 입력하세요.\n");
         return 1;
     }
 
-    for (int i = start; i <= end; i++) {
-        printf("==== %d 단 ====\n", i);
-        for (int j = 1; j <= 9; j++) {
-            printf("%2d x %2d = %2d\n", i, j, i * j);
-        }
-        printf("\n");
-    }
+    print_table(start, end); 
     return 0;
 }
